5c4934372bc03424a4632b37747727f04047fc02
Pedro Flores <pflores@dncp.gov.py>
2021-09-13 10:53:49 -0400

    simple example completed for doit-esp32-devkit-v1

--- suchi-firmware.ino
+++ suchi-firmware.ino
@@ -19,22 +19,27 @@ PMS::DATA data;
 void setup()
 {
   Serial.begin(9600);   // GPIO1, GPIO3 (TX/RX pin on ESP-12E Development Board)
-  Serial1.begin(9600);  // GPIO2 (D4 pin on ESP-12E Development Board)
+  Serial.println();
+  Serial.print("Air Quality Sensor starting up, v");
+  pmsSerial.begin(9600);   // Connection for PMS5003
+  // Serial1.begin(9600);  // GPIO2 (D4 pin on ESP-12E Development Board)
 }
 
 void loop()
 {
   if (pms.read(data))
   {
-    Serial1.print("PM 1.0 (ug/m3): ");
-    Serial1.println(data.PM_AE_UG_1_0);
+    Serial.print("PM 1.0 (ug/m3): ");
+    Serial.println(data.PM_AE_UG_1_0);
 
-    Serial1.print("PM 2.5 (ug/m3): ");
-    Serial1.println(data.PM_AE_UG_2_5);
+    Serial.print("PM 2.5 (ug/m3): ");
+    Serial.println(data.PM_AE_UG_2_5);
 
-    Serial1.print("PM 10.0 (ug/m3): ");
-    Serial1.println(data.PM_AE_UG_10_0);
+    Serial.print("PM 10.0 (ug/m3): ");
+    Serial.println(data.PM_AE_UG_10_0);
 
-    Serial1.println();
+    Serial.println();
   }
+  Serial.println("no data ?");
+  delay(1000);
 }
