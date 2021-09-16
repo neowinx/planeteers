bc260bf683de917d44fb0dca7c18644d463c38b1
Pedro Flores <pflores@dncp.gov.py>
2021-09-16 16:01:18 -0400

    cleannup

--- ma-ti-firmware/ma-ti-firmware.ino
+++ ma-ti-firmware/ma-ti-firmware.ino
@@ -7,16 +7,19 @@ uint16_t  g_pm1p0_sp_value      = 0;  // Standard Particle calibration pm1.0 rea
 uint16_t  g_pm2p5_sp_value      = 0;  // Standard Particle calibration pm2.5 reading
 uint16_t  g_pm10p0_sp_value     = 0;  // Standard Particle calibration pm10.0 reading
 
-uint16_t  g_pm1p0_ae_value      = 0;  // Atmospheric Environment pm1.0 reading
-uint16_t  g_pm2p5_ae_value      = 0;  // Atmospheric Environment pm2.5 reading
-uint16_t  g_pm10p0_ae_value     = 0;  // Atmospheric Environment pm10.0 reading
+// ** Not need to store all these other values since they are not used in Linka
+//    but we will mantain these comments in case they are needed in the future
 
-uint32_t  g_pm0p3_ppd_value     = 0;  // Particles Per Deciliter pm0.3 reading
-uint32_t  g_pm0p5_ppd_value     = 0;  // Particles Per Deciliter pm0.5 reading
-uint32_t  g_pm1p0_ppd_value     = 0;  // Particles Per Deciliter pm1.0 reading
-uint32_t  g_pm2p5_ppd_value     = 0;  // Particles Per Deciliter pm2.5 reading
-uint32_t  g_pm5p0_ppd_value     = 0;  // Particles Per Deciliter pm5.0 reading
-uint32_t  g_pm10p0_ppd_value    = 0;  // Particles Per Deciliter pm10.0 reading
+// uint16_t  g_pm1p0_ae_value      = 0;  // Atmospheric Environment pm1.0 reading
+// uint16_t  g_pm2p5_ae_value      = 0;  // Atmospheric Environment pm2.5 reading
+// uint16_t  g_pm10p0_ae_value     = 0;  // Atmospheric Environment pm10.0 reading
+// 
+// uint32_t  g_pm0p3_ppd_value     = 0;  // Particles Per Deciliter pm0.3 reading
+// uint32_t  g_pm0p5_ppd_value     = 0;  // Particles Per Deciliter pm0.5 reading
+// uint32_t  g_pm1p0_ppd_value     = 0;  // Particles Per Deciliter pm1.0 reading
+// uint32_t  g_pm2p5_ppd_value     = 0;  // Particles Per Deciliter pm2.5 reading
+// uint32_t  g_pm5p0_ppd_value     = 0;  // Particles Per Deciliter pm5.0 reading
+// uint32_t  g_pm10p0_ppd_value    = 0;  // Particles Per Deciliter pm10.0 reading
 
 void setup()
 {
@@ -43,42 +46,21 @@ void receivePayload(char payload[]) {
 /*
   Update particulate matter sensor values
 */
-void updatePmsReadings(char payload[])
-{
-  char* values = strtok(payload, ":");
-  // Put the most recent values into globals for reference elsewhere
-  g_pm1p0_sp_value   = values[0];
-  g_pm2p5_sp_value   = values[1]; 
-  g_pm10p0_sp_value  = values[2]; 
-
-  g_pm1p0_ae_value   = values[3]; 
-  g_pm2p5_ae_value   = values[4]; 
-  g_pm10p0_ae_value  = values[5]; 
-
-  uint32_t  new_pm0p3_ppd_value  = values[6]; 
-  uint32_t  new_pm0p5_ppd_value  = values[7]; 
-  uint32_t  new_pm1p0_ppd_value  = values[8]; 
-  uint32_t  new_pm2p5_ppd_value  = values[9]; 
-  uint32_t  new_pm5p0_ppd_value  = values[10]; 
-  uint32_t  new_pm10p0_ppd_value = values[11];
-
-  // This condition below should NOT be required, but currently I get all
-  // 0 values for the PPD results every second time. This check only updates
-  // the global values if there is a non-zero result for any of the values:
-  if (
-    new_pm0p3_ppd_value +
-    new_pm0p5_ppd_value +
-    new_pm1p0_ppd_value +
-    new_pm2p5_ppd_value +
-    new_pm5p0_ppd_value +
-    new_pm10p0_ppd_value
-      != 0)
+void updatePmsReadings(char payload[]) {
+  // Split the value in two strs
+  char* separator = strchr(payload, ':');
+  while (separator != 0)
   {
-    g_pm0p3_ppd_value  = new_pm0p3_ppd_value;
-    g_pm0p5_ppd_value  = new_pm0p5_ppd_value;
-    g_pm1p0_ppd_value  = new_pm1p0_ppd_value;
-    g_pm2p5_ppd_value  = new_pm2p5_ppd_value;
-    g_pm5p0_ppd_value  = new_pm5p0_ppd_value;
-    g_pm10p0_ppd_value = new_pm10p0_ppd_value;
+    ++separator;
+    int position = atoi(separator);
+
+    // Do something with servoId and position
   }
+  // Find the next value in payload string
+  value = strchr(0, ":");
+
+  // Put the most recent values into globals for reference elsewhere
+  g_pm1p0_sp_value   = atoi(values[0]);
+  g_pm2p5_sp_value   = atoi(values[1]); 
+  g_pm10p0_sp_value  = atoi(values[2]); 
 }
--- suchi-firmware/suchi-firmware.ino
+++ suchi-firmware/suchi-firmware.ino
@@ -6,19 +6,21 @@ uint16_t  g_pm1p0_sp_value      = 0;  // Standard Particle calibration pm1.0 rea
 uint16_t  g_pm2p5_sp_value      = 0;  // Standard Particle calibration pm2.5 reading
 uint16_t  g_pm10p0_sp_value     = 0;  // Standard Particle calibration pm10.0 reading
 
-uint16_t  g_pm1p0_ae_value      = 0;  // Atmospheric Environment pm1.0 reading
-uint16_t  g_pm2p5_ae_value      = 0;  // Atmospheric Environment pm2.5 reading
-uint16_t  g_pm10p0_ae_value     = 0;  // Atmospheric Environment pm10.0 reading
+// ** Not need to send all these other values since they are not used in Linka
+//    but we will mantain these comments in case they are needed in the future
 
-uint32_t  g_pm0p3_ppd_value     = 0;  // Particles Per Deciliter pm0.3 reading
-uint32_t  g_pm0p5_ppd_value     = 0;  // Particles Per Deciliter pm0.5 reading
-uint32_t  g_pm1p0_ppd_value     = 0;  // Particles Per Deciliter pm1.0 reading
-uint32_t  g_pm2p5_ppd_value     = 0;  // Particles Per Deciliter pm2.5 reading
-uint32_t  g_pm5p0_ppd_value     = 0;  // Particles Per Deciliter pm5.0 reading
-uint32_t  g_pm10p0_ppd_value    = 0;  // Particles Per Deciliter pm10.0 reading
+// uint16_t  g_pm1p0_ae_value      = 0;  // Atmospheric Environment pm1.0 reading
+// uint16_t  g_pm2p5_ae_value      = 0;  // Atmospheric Environment pm2.5 reading
+// uint16_t  g_pm10p0_ae_value     = 0;  // Atmospheric Environment pm10.0 reading
+// 
+// uint32_t  g_pm0p3_ppd_value     = 0;  // Particles Per Deciliter pm0.3 reading
+// uint32_t  g_pm0p5_ppd_value     = 0;  // Particles Per Deciliter pm0.5 reading
+// uint32_t  g_pm1p0_ppd_value     = 0;  // Particles Per Deciliter pm1.0 reading
+// uint32_t  g_pm2p5_ppd_value     = 0;  // Particles Per Deciliter pm2.5 reading
+// uint32_t  g_pm5p0_ppd_value     = 0;  // Particles Per Deciliter pm5.0 reading
+// uint32_t  g_pm10p0_ppd_value    = 0;  // Particles Per Deciliter pm10.0 reading
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   Serial.println();
   Serial.print("Suchi Air Quality Sensor starting up, v");
@@ -27,46 +29,15 @@ void setup()
   initLoRa();
 }
 
-void loop()
-{
-  if (pmsRead())
-  {
+void loop() {
+  if (pmsRead()) {
     g_pm1p0_sp_value   = data.PM_SP_UG_1_0;
     g_pm2p5_sp_value   = data.PM_SP_UG_2_5;
     g_pm10p0_sp_value  = data.PM_SP_UG_10_0;
 
-    g_pm1p0_ae_value   = data.PM_AE_UG_1_0;
-    g_pm2p5_ae_value   = data.PM_AE_UG_2_5;
-    g_pm10p0_ae_value  = data.PM_AE_UG_10_0;
-
-    // This condition below should NOT be required, but currently I get all
-    // 0 values for the PPD results every second time. This check only updates
-    // the global values if there is a non-zero result for any of the values:
-    if (data.PM_TOTALPARTICLES_0_3 + data.PM_TOTALPARTICLES_0_5
-        + data.PM_TOTALPARTICLES_1_0 + data.PM_TOTALPARTICLES_2_5
-        + data.PM_TOTALPARTICLES_5_0 + data.PM_TOTALPARTICLES_10_0
-        != 0)
-    {
-      g_pm0p3_ppd_value  = data.PM_TOTALPARTICLES_0_3;
-      g_pm0p5_ppd_value  = data.PM_TOTALPARTICLES_0_5;
-      g_pm1p0_ppd_value  = data.PM_TOTALPARTICLES_1_0;
-      g_pm2p5_ppd_value  = data.PM_TOTALPARTICLES_2_5;
-      g_pm5p0_ppd_value  = data.PM_TOTALPARTICLES_5_0;
-      g_pm10p0_ppd_value = data.PM_TOTALPARTICLES_10_0;
-    }
-
     String _message = g_pm1p0_sp_value + ":"
       + g_pm2p5_sp_value + ":" 
-      + g_pm10p0_sp_value + ":" 
-      + g_pm1p0_ae_value + ":" 
-      + g_pm2p5_ae_value + ":" 
-      + g_pm10p0_ae_value + ":" 
-      + g_pm0p3_ppd_value + ":" 
-      + g_pm0p5_ppd_value + ":" 
-      + g_pm1p0_ppd_value + ":" 
-      + g_pm2p5_ppd_value + ":" 
-      + g_pm5p0_ppd_value + ":" 
-      + g_pm10p0_ppd_value;
+      + g_pm10p0_sp_value;
     
     Serial.print("PM 1.0 (ug/m3): ");
     Serial.println(data.PM_AE_UG_1_0);
@@ -80,9 +51,11 @@ void loop()
     Serial.println();
 
     sendLoRa(_message);
-    
+  } else {
+    Serial.println("no data ?");
   }
-  Serial.println("no data ?");
-  delay(20000);
+
+  // seconds to delay for the next attempt to read the values (by default its 30)
+  delay(30000);
 }
 
